e9c82f44a58021b864e03465bde79ea543bc41d3
joeyntut <40187978+JoeyChen-NTUT@users.noreply.github.com>
2018-10-31 11:19:58 +0800

    Update create_yda_docker.sh

--- create_yda_docker.sh
+++ create_yda_docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export YDA_SESSION=$(printf "%02d" $1)
 export YDA_SESSION_NAME=yda-$YDA_ALIAS_$YDA_SESSION
-export YDA_DIR=/usr/local/yda/session
+export YDA_ROOT=/usr/local/yda
+export YDA_DIR=$YDA_ROOT/session
 export YDA_ALIAS_DIR=$YDA_DIR/$YDA_SESSION_NAME
 export RND_PASS=$(pwgen -s 20)
 
@@ -46,6 +47,7 @@ echo "Waiting 15 second for MySQL Service is Active...."
 sleep 15
 
 echo "run post command"
+echo "docker-compose post work"
 docker-compose exec web mkdir -p /var/www/html/storage/app/media
 docker-compose exec web touch -d '1 Jan 2018 00:00' /var/www/html/storage/app/media/gpvip.csv
 docker-compose exec web php artisan october:up
@@ -55,8 +57,11 @@ docker-compose exec web php artisan config:clear
 docker-compose exec web php artisan config:cache
 docker-compose exec web php artisan cache:clear
 docker-compose exec web chown -R www-data:www-data /var/www/html
+
+echo "add web to nginx network"
 docker network connect nginx $YDA_SESSION_NAME-web
 
+echo "modify web configs"
 docker cp $YDA_SESSION_NAME-web:/var/www/html/.htaccess /tmp/.htaccess
 sed -i "s/# RewriteBase \//RewriteBase \/$YDA_SESSION/g" /tmp/.htaccess
 docker cp /tmp/.htaccess $YDA_SESSION_NAME-web:/var/www/html/.htaccess
@@ -66,3 +71,16 @@ sed -i "1a\ \ \ \ \ \ \ \ Alias \"\/$YDA_SESSION\" \"\/var\/www\/html\"" /tmp/00
 docker cp /tmp/000-default.conf $YDA_SESSION_NAME-web:/etc/apache2/sites-available/000-default.conf
 rm -f /tmp/000-default.conf
 docker exec $YDA_SESSION_NAME-web service apache2 reload
+
+echo "deploy nginx config"
+YDA_NGINX_PATH=$YDA_ROOT/nginx
+YDA_NGINX_CONF_PATH=$YDA_NGINX_PATH/conf.d
+YDA_NGINX_SESSION_CONF=$YDA_NGINX_CONF_PATH/$YDA_SESSION_NAME.conf
+
+if [ ! -f $YDA_NGINX_CONF_PATH/yda-default.conf ]; then
+    cp nginx-yda-default $YDA_NGINX_CONF_PATH/yda-default.conf
+fi
+mkdir -p $YDA_NGINX_CONF_PATH/yda
+
+cp nginx-yda.template $YDA_NGINX_SESSION_CONF
+sed -i "s/SESSION_NUMBER/$YDA_SESSION/g" $YDA_NGINX_SESSION_CONF
